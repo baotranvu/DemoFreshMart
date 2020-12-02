2eab8dc6cd1165fbb4eac0d03c360b8a173e0445
Bao <tranvubao195@gmail.com>
2020-12-02 08:52:00 +0700

    Create Interfaces

---
+++ Interfaces/ICustomerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace ViewModels
+{
+    public interface ICustomerViewModel
+    {
+        BindingSource CustomerBindingSource { get; set; }
+        string Title { get; }
+        event PropertyChangedEventHandler PropertyChanged;
+        void Initialize();
+        void New();
+        void Delete();
+        void Save();
+        void Load();
+      
+    }
+}
---
+++ ViewModels/CustomerViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace ViewModels
+{
+    class CustomerViewModel : INotifyPropertyChanged, ICustomerViewModel
+    {
+        public BindingSource CustomerBindingSource
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void New()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
